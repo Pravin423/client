2029aa8a82d19ead476be5cfa5c239ad95db7f72
Pravin423 <pravingouda00@gmail.com>
2026-01-21 12:27:43 +0530

    sss

--- src/pages/employee/dashboard.js
+++ src/pages/employee/dashboard.js
@@ -1,12 +1,36 @@
-import ProtectedRoute from "../../components/ProtectedRoute";
 
-export default function  EmployeeDashboard() {
-  return (
-    <ProtectedRoute allowedRoles={["employee"]}>
-      <div className="p-6">
-        <h1 className="text-2xl font-bold">Employee Dashboard</h1>
-        <p>Only employee can see this.</p>
+
+import ProtectedRoute from "../../components/ProtectedRoute";
+import { useRouter } from "next/router";
+import { logoutUser } from "@/utils/auth";
+import Router from "next/router";
+
+export default function EmployeeDashboard() {
+ return (
+  <ProtectedRoute allowedRoles={["employee"]}>
+    <div className="p-6">
+      
+      {/* Header */}
+      <div className="flex items-center justify-between mb-6">
+        <div>
+          <h1 className="text-2xl font-bold">Employee Dashboard</h1>
+          <p className="text-gray-600">Only employee can see this.</p>
+        </div>
+
+        {/* Logout Button */}
+      <button
+          onClick={async () => {
+            await logoutUser();
+            Router.replace("/login");
+          }}
+          className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition"
+        >
+          Logout
+        </button>
       </div>
-    </ProtectedRoute>
-  );
+
+    </div>
+  </ProtectedRoute>
+);
+
 }
--- src/pages/manager/dashboard.js
+++ src/pages/manager/dashboard.js
@@ -1,12 +1,36 @@
-import ProtectedRoute from "../../components/ProtectedRoute";
 
-export default function  ManagerDashboard() {
-  return (
-    <ProtectedRoute allowedRoles={["manager"]}>
-      <div className="p-6">
-        <h1 className="text-2xl font-bold">Manager Dashboard</h1>
-        <p>Only manager can see this.</p>
+
+import ProtectedRoute from "../../components/ProtectedRoute";
+import { useRouter } from "next/router";
+import { logoutUser } from "@/utils/auth";
+import Router from "next/router";
+
+export default function ManagerDashboard() {
+ return (
+  <ProtectedRoute allowedRoles={["manager"]}>
+    <div className="p-6">
+      
+      {/* Header */}
+      <div className="flex items-center justify-between mb-6">
+        <div>
+          <h1 className="text-2xl font-bold">manager Dashboard</h1>
+          <p className="text-gray-600">Only manager can see this.</p>
+        </div>
+
+        {/* Logout Button */}
+      <button
+          onClick={async () => {
+            await logoutUser();
+            Router.replace("/login");
+          }}
+          className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition"
+        >
+          Logout
+        </button>
       </div>
-    </ProtectedRoute>
-  );
+
+    </div>
+  </ProtectedRoute>
+);
+
 }
